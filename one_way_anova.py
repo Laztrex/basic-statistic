701450bd78c384605e9223e82b2faae3a9a8acb5
Alexey Lazarev <googlaz1@gmail.com>
2020-02-11 00:41:28 +0300

    добавил таблицу, метод вычисления через стат.пакет f,p-value

--- one_way_anova.py
+++ one_way_anova.py
@@ -14,7 +14,7 @@ class Anova:
         self.groups = {}
         self.file = file_for_analyze
         self.sum_mean = 0
-        self.value_ssb, self.value_ssw, self.sst = 0, 0, 0
+        self.value_ssb, self.value_ssw, self.sst, self.f, self.p = 0, 0, 0, 0, 0
 
     def open_file(self):
         with open(self.file, 'r', newline='') as csv_file:
@@ -41,8 +41,9 @@ class Anova:
             self.groups[group].update({'sd': sqrt(self.ssw(values["mean"], mean) / lenght - 1)})
         print(self.groups)
         self.ssb(total_mean / n)
-        self.f_value(n)
+        self.f = self.f_value(n)
         print(self.value_ssw)
+        self.beautiful_made_table()
 
     def ssb(self, mean_gr):
         for i in self.groups.values():
@@ -58,7 +59,8 @@ class Anova:
         return p
 
     def p_value(self, f, dfb, dfw):
-        return sp.f.sf(f, dfb, dfw)
+        self.p = sp.f.sf(f, dfb, dfw)
+        return self.p
 
     def f_value(self, n):
         f = (self.value_ssb / (len(self.groups) - 1)) / (self.value_ssw / (n - len(self.groups)))
@@ -67,19 +69,32 @@ class Anova:
         print(f'p-value - {self.p_value(f, len(self.groups) - 1, n - len(self.groups))}')
         return f
 
+    def beautiful_made_table(self):
+        print(f'{"+":-<9}{"+":-<5}{"+":-<20}{"+":-<6}{"+":->{24}}\n'
+              f'| {"группа":^} | {"df":^} | {"mean":^{17}} | {"sd":^{26}} |\n'
+              f'{"+":-<9}{"+":-<5}{"+":-<20}{"+":-<6}{"+":->{24}}')
+        for i, j in self.groups.items():
+            print(f'| {i:^6} | {j["df"]:^{2}} | {j["mean"]:^{17}} | {j["sd"]:^{26}} |')
+        print(f'{"+":-<9}{"+":-^}{"+":->{20}}{"+":->{34}}\n'
+              f'| {"f-value":^}| {self.f:^} | {"p-value":^} | {self.p:^}|\n'
+              f'{"+":-<9}{"+":-^}{"+":->{20}}{"+":->{34}}')
+
+    def gracefully_with_stat_packages(self):
+        data = pd.read_csv('genetherapy.csv', sep=',')
+
+        groups = pd.unique(data.Therapy.values)
+        dict_data = {group: data['expr'][data.Therapy == group] for group in groups}
+
+        F_value, p_value = sp.f_oneway(dict_data["A"], dict_data["B"], dict_data["C"], dict_data["D"])
+        print(F_value, p_value)
+
 
 if __name__ == '__main__':
     my_statistic = Anova(file_for_analyze='genetherapy.csv')
     my_statistic.open_file()
 
 # ===== on PANDAS =====
-# data = pd.read_csv('genetherapy.csv', sep=',')
-#
-# groups = pd.unique(data.Therapy.values)
-# dict_data = {group: data['expr'][data.Therapy == group] for group in groups}
-#
-# F_value, p_value = sp.f_oneway(dict_data["A"], dict_data["B"], dict_data["C"], dict_data["D"])
-# print(F_value, p_value)
+
 
 # TODO задействовать collections для работы со словарями
 # TODO после освоения расчетов статистических перенести работу на статистические пакеты
